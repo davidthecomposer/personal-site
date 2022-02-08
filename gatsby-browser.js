d06bdb5781d83fbfc79245f5eba0bd3343c74a22
David Reform Collective <davidreformcollective@MacBook-Pro.local>
2022-02-08 09:46:24 -0700

    assets

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,5 @@
 import "fonts/reset.css"
 import "fonts/typography.css"
-import React from "react"
 import gsap from "gsap"
 
 import { ScrollTrigger } from "gsap/ScrollTrigger"
@@ -8,7 +7,6 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin"
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin"
 import ScrollToPlugin from "gsap/ScrollToPlugin"
 import { CustomEase } from "gsap/CustomEase"
-import ContextStore from "components/ContextStore"
 
 gsap.registerPlugin(
   ScrollTrigger,
@@ -17,9 +15,3 @@ gsap.registerPlugin(
   MorphSVGPlugin,
   CustomEase
 )
-
-// export const wrapPageElement = ({ element, props }) => {
-//   // props provide same data to Layout as Page element will get
-//   // including location, data, etc - you don't need to pass it
-//   return <ContextStore {...props}>{element}</ContextStore>
-// }
